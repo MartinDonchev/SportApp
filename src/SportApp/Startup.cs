6580c65fab2d73e50943f950f1906b0507e09789
MartinDonchev <donchev_m@abv.bg>
2018-12-30 23:32:15 +0200

    Update to version 2.2

--- src/SportApp/Startup.cs
+++ src/SportApp/Startup.cs
@@ -51,7 +51,7 @@ namespace SportApp
                 )
                 .AddEntityFrameworkStores<SportAppContext>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
