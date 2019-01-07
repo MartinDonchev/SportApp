6dab1ba1d4f57e6fac17fbfd69365881d2f03d32
MartinDonchev <donchev_m@abv.bg>
2019-01-07 23:57:51 +0200

    Making Area Stadiums

---
+++ src/SportApp/Areas/Stadiums/Controllers/StadiaController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SportApp.Data.Models;
+using SportApp.Models;
+
+namespace SportApp.Areas.Stadiums.Controllers
+{
+    [Area("Stadiums")]
+    public class StadiaController : Controller
+    {
+        private readonly SportAppContext _context;
+
+        public StadiaController(SportAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Stadiums/Stadia
+        public async Task<IActionResult> Index()
+        {
+            var sportAppContext = _context.Stadiums.Include(s => s.Team);
+            return View(await sportAppContext.ToListAsync());
+        }
+
+        // GET: Stadiums/Stadia/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stadium = await _context.Stadiums
+                .Include(s => s.Team)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (stadium == null)
+            {
+                return NotFound();
+            }
+
+            return View(stadium);
+        }
+
+        // GET: Stadiums/Stadia/Create
+        public IActionResult Create()
+        {
+            ViewData["TeamId"] = new SelectList(_context.Teams, "Id", "Name");
+            return View();
+        }
+
+        // POST: Stadiums/Stadia/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Capacity,TeamId")] Stadium stadium)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(stadium);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeamId"] = new SelectList(_context.Teams, "Id", "Name", stadium.TeamId);
+            return View(stadium);
+        }
+
+        // GET: Stadiums/Stadia/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stadium = await _context.Stadiums.FindAsync(id);
+            if (stadium == null)
+            {
+                return NotFound();
+            }
+            ViewData["TeamId"] = new SelectList(_context.Teams, "Id", "Name", stadium.TeamId);
+            return View(stadium);
+        }
+
+        // POST: Stadiums/Stadia/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Capacity,TeamId")] Stadium stadium)
+        {
+            if (id != stadium.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(stadium);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StadiumExists(stadium.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeamId"] = new SelectList(_context.Teams, "Id", "Name", stadium.TeamId);
+            return View(stadium);
+        }
+
+        // GET: Stadiums/Stadia/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stadium = await _context.Stadiums
+                .Include(s => s.Team)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (stadium == null)
+            {
+                return NotFound();
+            }
+
+            return View(stadium);
+        }
+
+        // POST: Stadiums/Stadia/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var stadium = await _context.Stadiums.FindAsync(id);
+            _context.Stadiums.Remove(stadium);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StadiumExists(int id)
+        {
+            return _context.Stadiums.Any(e => e.Id == id);
+        }
+    }
+}
--- src/SportApp/Startup.cs
+++ src/SportApp/Startup.cs
@@ -79,6 +79,10 @@ namespace SportApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Stadia}/{action=Index}/{id?}");
             });
         }
     }
