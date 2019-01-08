f3db0f62b3ca5a98556a747596089d0f60ba59fe
MartinDonchev <donchev_m@abv.bg>
2019-01-08 23:54:19 +0200

    Add Matches Controller

---
+++ src/SportApp/Controllers/MatchesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SportApp.Data.Models;
+using SportApp.Models;
+
+namespace SportApp.Controllers
+{
+    public class MatchesController : Controller
+    {
+        private readonly SportAppContext _context;
+
+        public MatchesController(SportAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Matches
+        public async Task<IActionResult> Index()
+        {
+            var sportAppContext = _context.Matches.Include(m => m.AwayTeam).Include(m => m.HomeTeam);
+            return View(await sportAppContext.ToListAsync());
+        }
+
+        // GET: Matches/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var match = await _context.Matches
+                .Include(m => m.AwayTeam)
+                .Include(m => m.HomeTeam)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+
+            return View(match);
+        }
+
+        // GET: Matches/Create
+        public IActionResult Create()
+        {
+            ViewData["AwayTeamId"] = new SelectList(_context.Teams, "Id", "Name");
+            ViewData["HomeTeamId"] = new SelectList(_context.Teams, "Id", "Name");
+            return View();
+        }
+
+        // POST: Matches/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,HomeTeamId,AwayTeamId,DateOfMatch,Referee")] Match match)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(match);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AwayTeamId"] = new SelectList(_context.Teams, "Id", "Name", match.AwayTeamId);
+            ViewData["HomeTeamId"] = new SelectList(_context.Teams, "Id", "Name", match.HomeTeamId);
+            return View(match);
+        }
+
+        // GET: Matches/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var match = await _context.Matches.FindAsync(id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+            ViewData["AwayTeamId"] = new SelectList(_context.Teams, "Id", "Name", match.AwayTeamId);
+            ViewData["HomeTeamId"] = new SelectList(_context.Teams, "Id", "Name", match.HomeTeamId);
+            return View(match);
+        }
+
+        // POST: Matches/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,HomeTeamId,AwayTeamId,DateOfMatch,Referee")] Match match)
+        {
+            if (id != match.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(match);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MatchExists(match.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AwayTeamId"] = new SelectList(_context.Teams, "Id", "Name", match.AwayTeamId);
+            ViewData["HomeTeamId"] = new SelectList(_context.Teams, "Id", "Name", match.HomeTeamId);
+            return View(match);
+        }
+
+        // GET: Matches/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var match = await _context.Matches
+                .Include(m => m.AwayTeam)
+                .Include(m => m.HomeTeam)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+
+            return View(match);
+        }
+
+        // POST: Matches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var match = await _context.Matches.FindAsync(id);
+            _context.Matches.Remove(match);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MatchExists(int id)
+        {
+            return _context.Matches.Any(e => e.Id == id);
+        }
+    }
+}
